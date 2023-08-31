36cc4245241da4231aa5b66136c90d075b7229b4
Nacho <ignacio_coletta@hotmail.com>
2023-08-31 17:24:01 -0300

    first commit

---
+++ src/main/main.java
@@ -0,0 +1,17 @@
+
+package main;
+
+import views.LoginView;
+
+/**
+ *
+ * @author ignac
+ */
+public class main {
+    public static void main(String[] args) {
+        // instancia del login
+        
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+}
